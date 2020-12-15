3c3cad700a401171910b4983092fbccc0ba7aff3
jaeou <jaeout2@gmail.com>
2020-12-15 21:34:26 +0900

    Test Commit

--- SelfStudy/src/javaStudy/test.java
+++ SelfStudy/src/javaStudy/test.java
@@ -4,6 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("commit test");
 		System.out.println("커밋 테스트 중");
+		
+		System.out.println("변경사항 테스트중");
 	}
 
 }
