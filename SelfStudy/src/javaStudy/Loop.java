e8cfe471e8fd17e9ac158606fa28e76219ea8e85
jaeou <jaeout2@gmail.com>
2020-12-17 22:16:49 +0900

    반복문

--- SelfStudy/src/javaStudy/Loop.java
+++ SelfStudy/src/javaStudy/Loop.java
@@ -47,5 +47,52 @@ public class Loop {
 	} //이 코드는 i값을 증가시키고 증가된 i값을 화면에 출력한다.
 	System.out.println();
 	
+	
+	//작업문
+/*
+ 작업문이 하나의 문장인 경우 중괄호({})가 필요없지만 여러 개의 문장으로 구성되는
+ 경우 반드시 중괄호({})로 묶어야 한다. 
+*/
+
+	//for문 활용  _ 0에서 9까지 정수 출력
+
+	for(i=0; i<10; i++) {
+		System.out.print(i);
+	}
+	
+	//위의 문장은 중괄호 생략해도 가능하다
+	System.out.println();
+	for(i=0; i<10; i++)
+		System.out.print(i);
+	System.out.println();
+	
+	
+//반복문에 변수 선언
+//변수 j를 for무누 블록 내에서만 사용되는 지역변수로 선언하여 사용할 수 있다
+//이때 변수 j는 for문 밖에서는 사용할 수 없다.
+	for(int j=0; j<10; j++) //변수 j는 for문 밖에서 사용 불가
+		System.out.print(j);
+	
+	System.out.println();
+	
+	
+	
+	
+	//1에서 100까지의 합 구하기 3가지 사례 
+	int sum = 0; 
+	for(i=1; i<=100; i++) //1에서 100끼지 반복
+		sum += i; 
+	System.out.println(sum);
+	
+	sum = 0;
+	for(i=100; i>=1; i--) //100에서 1까지 반복
+		sum += i; 
+	System.out.println(sum);
+	
+	int i2, sum2; 
+	for (i2=1, sum2=0; i2<=100; i2++) //sum2 변수를 0으로 초기화 
+		sum2 += i2;
+	System.out.println(sum2);
+	
 	}
 }
