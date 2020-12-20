2d5d37980618b611d48c60799e0550b11621f9eb
jaeou <jaeout2@gmail.com>
2020-12-20 21:32:24 +0900

    배열과 for-each문

---
+++ SelfStudy/src/javaStudy/Ex3_9.java
@@ -0,0 +1,18 @@
+package javaStudy;
+//배열과 for-each문
+
+/*
+기존의 for문을 변형하여, 배열이나 나열(enumeration)의 
+크기만큼 루프를 돌면서 각 원소를 순차적으로 접근하는데 유용하게
+만든 for문을 for-each문(오라클에서는 advanced for라고 부름)
+이라고 부르며, 구조는 다음과 같다
+
+ for(변수 : 배열레퍼런스) {
+       ..반복 작업문..
+  }
+  
+ */
+
+public class Ex3_9 {
+
+}
