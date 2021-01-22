4db1fd7249d72cc3e20e52821a3bd6538a6241c4
jaeou <jaeout2@gmail.com>
2021-01-22 23:04:29 +0900

    상속

--- SelfStudy/src/javaStudy/ColorPointEx.java
+++ SelfStudy/src/javaStudy/ColorPointEx.java
@@ -1,5 +1,15 @@
 package javaStudy;
 
+
+/*
+자바에서는 부모 클래스를 슈퍼클래스(super class), 
+상속받는 자식 클래스를 서브클래스(sub class)라고 부르며,
+상속을 선언할 때 확장한다는 뜻을 가진 extends 키워드를 사용한다 
+
+부모클래스의 멤버를 물려받는 자식클래스는, 부모클래스에 선언된
+필드나 메소드를 다시 반복하여 작성할 필요가 없고,
+필드나 메소드를 추가 작성하면 된다.
+ */
 class Point{
 	private int x,y; //한 점을 구성하는 x,y 좌표
 	public void set(int x, int y) {
