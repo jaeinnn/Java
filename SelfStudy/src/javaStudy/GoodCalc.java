597e1a7f7824a242f107d9115bca1e172ff42460
jaeout <jaeout2@gmail.com>
2021-04-30 17:15:22 +0900

    예제 5-7

---
+++ SelfStudy/src/javaStudy/GoodCalc.java
@@ -0,0 +1,41 @@
+package javaStudy;
+
+//예제 5-7
+//다음 추상 클래스 Calculator를 상속받은 GoodCalc 클래스를 구현하라
+abstract class Calculator{
+	public abstract int add(int a, int b); //두 정수의 합을 구하여 리턴
+	public abstract int substract(int a, int b); //두 정수의 차를 구하여 리턴
+	public abstract double average(int[] a); //배열에 저장된 정수의 평균 리턴
+}
+
+public class GoodCalc extends Calculator {
+	
+	@Override
+	public int add(int a, int b) { //추상메소드 구현
+		return a + b; 
+	}
+
+	@Override
+	public int substract(int a, int b) { //추상메소드 구현
+		return a - b; 
+	}
+
+	@Override
+	public double average(int[] a) { //추상메소드 구현
+		double sum = 0; 
+		for (int i = 0; i<a.length; i++)
+			sum += a[i]; 
+		return sum/a.length; 
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GoodCalc c = new GoodCalc(); 
+		System.out.println(c.add(2, 3));
+		System.out.println(c.substract(2, 3));
+		System.out.println(c.average(new int[] {2,3,4}));
+		System.out.println(c.average(new int[] {5,2}));
+		
+	}
+}
