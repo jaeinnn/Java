281716d8b8c6f1c22ce6b8a97913a93d11a05899
jaeout <jaeout2@gmail.com>
2021-07-14 09:49:07 +0900

    예제 8-2

---
+++ SelfStudy/src/javaStudy/FileReadSuccess.java
@@ -0,0 +1,30 @@
+package javaStudy;
+import java.io.*;
+
+public class FileReadSuccess {
+	public static void main(String[] args) {
+		
+		InputStreamReader in = null; 
+		FileInputStream fin = null; 
+		
+		try {
+			fin = new FileInputStream("C:\\Users\\KJI-PC\\Desktop\\References\\InputStreamReaderTest.txt");
+			in = new InputStreamReader(fin,"UTF-8");
+			
+			int c;
+			
+			System.out.println("인코딩 문자 집합은 " + in.getEncoding());
+			while((c = in.read()) != -1) {
+				System.out.print((char)c);		
+			}
+			
+			in.close();
+			fin.close();
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+		
+		
+	}
+
+}
