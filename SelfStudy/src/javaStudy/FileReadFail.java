88bc80d8514898638a519f02ad1568acbde7ec65
jaeout <jaeout2@gmail.com>
2021-07-15 13:06:59 +0900

    예제 8-3

---
+++ SelfStudy/src/javaStudy/FileReadFail.java
@@ -0,0 +1,26 @@
+package javaStudy;
+import java.io.*;
+
+public class FileReadFail {
+	public static void main(String[] args) {
+		
+		InputStreamReader in = null; 
+		FileInputStream fin = null; 
+		
+		try {
+			fin = new FileInputStream("C:\\Users\\KJI-PC\\Desktop\\References\\InputStreamReaderTest.txt");
+			in = new InputStreamReader(fin,"US-ASCII");
+			
+			int c; 
+			System.out.println("인코딩 문자 집합은 " + in.getEncoding());
+			while((c = in.read()) != -1) {
+				System.out.print((char)c);
+			}
+			in.close();
+			fin.close();
+	
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+	}
+}
